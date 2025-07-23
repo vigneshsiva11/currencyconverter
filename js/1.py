bd504f1d027fce9b5a9f3ad293e1ce16efaf15c3
Vignesh <149236000+vigneshsiva11@users.noreply.github.com>
2025-07-23 22:06:57 +0530

    Update 1.py

--- js/1.py
+++ js/1.py
@@ -1,13 +1,1 @@
 
-num = int(input())
-onum = num
-rev = 0
-
-while onum >0:
-    digit = onum%10
-    rev = rev * 10 + digit
-    num = onum//10
-if(rev==num):
-    print(f"{num} is a palindrome")
-else: 
-    print(f"{num} is not a palindrome")
